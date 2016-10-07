e809f5bdda909ce440f660ec735ed730888e5310
JF <jf@whatson-web.com>
2016-10-07 16:23:16 +0200

    traduction

--- Form/UserType.php
+++ Form/UserType.php
@@ -9,90 +9,115 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 class UserType extends AbstractType
 {
 
-    private $roles;
+	private $roles;
 
-    public function __construct($options = array())
-    {
-        # store roles
-        $this->roles    = $options['roles'];
-        $this->roles    = $this->refactorRoles($this->roles);
+	public function __construct($options = array())
+	{
+		# store roles
+		$this->roles = $options['roles'];
+		$this->roles = $this->refactorRoles($this->roles);
+	}
 
-    }
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array                $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
 
+		$builder
+			->add(
+				'roles',
+				'choice',
+				array(
+					'label'    => 'Rôle(s) : ',
+					'attr'     => array('class' => 'form-control select2'),
+					'required' => true,
+					'multiple' => true,
+					'choices'  => $this->roles,
+				)
+			)
+			->add(
+				'firstname',
+				'text',
+				array(
+					'label' => 'Prénom',
+				)
+			)
+			->add(
+				'lastname',
+				'text',
+				array(
+					'label' => 'Nom',
+				)
+			)
+			->add(
+				'civility',
+				'choice',
+				array(
+					'label'   => 'Civilité',
+					'choices' => array(
+						'Mme.' => 'Madame',
+						'M.'   => 'Monsieur',
+					),
+				)
+			)
+			->add(
+				'email',
+				'email',
+				array(
+					'label' => 'Email',
+				)
+			);
+	}
 
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+	/**
+	 * @param OptionsResolverInterface $resolver
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults(
+			array(
+				'data_class' => 'APP\UserBundle\Entity\User',
+			)
+		);
+	}
 
-        $builder
-            ->add('roles', 'choice', array(
-                    'label' => 'Rôle(s) : ',
-                    'attr' => array('class' => 'form-control select2'),
-                    'required' => true,
-                    'multiple' => true,
-                    'choices' => $this->roles
-                ))
-            ->add('firstname', 'text', array('label' => 'prénom : '))
-            ->add('lastname', 'text', array('label' => 'nom : '))
-            ->add('civility', 'choice', array(
-                'label' => 'Civivilité : ',
-                'choices'   => array(
-                    'Mme.' => 'Madame',
-                    'M.' => 'Monsieur'
-                )
-            ))
-            ->add('email', 'email', array('label' => 'email : '))
-        ;
-    }
-    
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'APP\UserBundle\Entity\User'
-        ));
-    }
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'wh_user_create';
+	}
 
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'wh_user_create';
-    }
+	/**
+	 * @return array
+	 */
+	public function getDefaultOptions()
+	{
+		return array(
+			'roles' => null,
+		);
+	}
 
-    /**
-     * @return array
-     */
-    public function getDefaultOptions()
-    {
-        return array(
-            'roles' => null
-        );
-    }
+	/**
+	 * refactorRoles ne récupère que le niveau 0
+	 *
+	 * @param $originRoles
+	 *
+	 * @return array
+	 */
+	private function refactorRoles($originRoles)
+	{
+		$roles = array();
 
-    /**
-     * refactorRoles ne récupère que le niveau 0
-     * @param $originRoles
-     * @return array
-     */
-    private function refactorRoles($originRoles)
-    {
-        $roles = array();
+		// Add herited roles
+		foreach ($originRoles as $roleParent => $rolesHerit) {
 
-        // Add herited roles
-        foreach ($originRoles as $roleParent => $rolesHerit) {
+			$roles[$roleParent] = $roleParent;
+		}
 
-            $roles[$roleParent] = $roleParent;
-
-        }
-
-        return $roles;
-
-    }
+		return $roles;
+	}
 }
